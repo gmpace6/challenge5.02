b69d3ae14667c4f785757d19b34472af49a990e0
Garrett Pace <elderpace6@gmail.com>
2023-05-02 10:39:07 -0600

    3rd

--- decodeString.js
+++ decodeString.js
@@ -1,4 +1,4 @@
-// Write your code below this line
+// // Write your code below this line
 function decodeString(string) {
     let decoder = Number(string[0])
     console.log(decoder)
@@ -10,7 +10,27 @@ function decodeString(string) {
         let shifted = decoder + shift
         answer += cipher[shifted]
         console.log(answer)
+        if (letter = cipher.length - 1) {
+        continue
+        }
     }
-    return answer
+    // return answer
 }
-decodeString("1asdf")
+decodeString("2fcjjm")
+
+
+// const letters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+// const decodeCypher = (encoded) => {
+//     let decoded = ''
+//     let encodeArray = encoded.split('')
+//     const shifter = +encodeArray.shift()
+//     encodeArray.forEach(letter => {
+//         let indexShift = letter.findIndex(ele => ele === letter) + shifter
+//         if(indexShift > 25){
+//             indexShift -= 26
+//         }
+//         decoded += letters[indexShift]
+//     })
+//     return decoded
+// }
+// console.log(decodeCypher(("2fcjjm")))
