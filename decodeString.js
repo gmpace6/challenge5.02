65135e46dc77447cbbe66c20b5da44f99e67a7ae
Garrett Pace <elderpace6@gmail.com>
2023-05-02 10:38:49 -0600

    2nd

--- decodeString.js
+++ decodeString.js
@@ -1,4 +1,16 @@
 // Write your code below this line
-function decodeString() {
-    
+function decodeString(string) {
+    let decoder = Number(string[0])
+    console.log(decoder)
+    let answer = ''
+    let cipher = 'abcdefghijklmnopqrstuvwxyz'
+    for (let i = 1; i < string.length; i++) {
+        let letter = string[i]
+        let shift = cipher.search(letter)
+        let shifted = decoder + shift
+        answer += cipher[shifted]
+        console.log(answer)
+    }
+    return answer
 }
+decodeString("1asdf")
